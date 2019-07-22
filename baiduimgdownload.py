2e39a6043e93f5f01b876f4b7ced0b050a90a8c5
junchou9463 <33283189+junchou9463@users.noreply.github.com>
2019-07-22 21:15:05 +0800

    baiduimgdownload.py
    百度图片爬虫下载，注意json数据的转化，爬取的图片地址加密过，需要解码。

---
+++ baiduimgdownload.py
@@ -0,0 +1,93 @@
+# -*- coding:utf-8 -*-
+# Author:Jun
+import requests
+import os
+import re
+import urllib
+
+
+def getManyPages(keyword,pages):
+    params=[]
+    for i in range(30,30*pages+30,30):
+        params.append({
+                      'tn': 'resultjson_com',
+                      'ipn': 'rj',
+                      'ct': 201326592,
+                      'is': '',
+                      'fp': 'result',
+                      'queryWord': keyword,
+                      'cl': 2,
+                      'lm': -1,
+                      'ie': 'utf-8',
+                      'oe': 'utf-8',
+                      'adpicid': '',
+                      'st': -1,
+                      'z': '',
+                      'ic': 0,
+                      'word': keyword,
+                      's': '',
+                      'se': '',
+                      'tab': '',
+                      'width': '',
+                      'height': '',
+                      'face': 0,
+                      'istype': 2,
+                      'qc': '',
+                      'nc': 1,
+                      'fr': '',
+                      'pn': i,
+                      'rn': 30,
+                      'gsm': '1e',
+                      '1488942260214': ''
+                  })
+    url = 'https://image.baidu.com/search/acjson'
+    urls = []
+    for i in params:
+        urls.append(requests.get(url,params=i).json().get('data'))
+
+    return urls
+
+def  baidtu_uncomplie(url):
+    res = ''
+    c = ['_z2C$q', '_z&e3B', 'AzdH3F']
+    d= {'w':'a', 'k':'b', 'v':'c', '1':'d', 'j':'e', 'u':'f', '2':'g', 'i':'h', 't':'i', '3':'j', 'h':'k', 's':'l', '4':'m', 'g':'n', '5':'o', 'r':'p', 'q':'q', '6':'r', 'f':'s', 'p':'t', '7':'u', 'e':'v', 'o':'w', '8':'1', 'd':'2', 'n':'3', '9':'4', 'c':'5', 'm':'6', '0':'7', 'b':'8', 'l':'9', 'a':'0', '_z2C$q':':', '_z&e3B':'.', 'AzdH3F':'/'}
+    if(url==None or 'http' in url):
+        return url
+    else:
+        j= url
+        for m in c:
+            j=j.replace(m,d[m])
+        for char in j:
+            if re.match('^[a-w\d]+$',char):
+                char = d[char]
+            res= res+char
+        return res
+
+def getImg(dataList, localPath):
+
+    if not os.path.exists(localPath):  # 新建文件夹
+        os.mkdir(localPath)
+
+    x = 0
+    for list in dataList:
+        for i in list:
+            if i.get('objURL') != None:
+                print('正在下载：%s' % baidtu_uncomplie(i.get('objURL')))
+                k=0
+                try:
+                    ir = requests.get(baidtu_uncomplie(i.get('objURL')))
+                    k+=1
+                    if k>10:
+                        break
+                except :
+                    continue
+                open(localPath + '%d.jpg' % x, 'wb').write(ir.content)
+                x += 1
+            else:
+                print('图片链接不存在')
+
+if __name__ == '__main__':
+    # prompt = "Enter a string that search wanted: "
+    # command = input(prompt)
+    dataList = getManyPages('迪丽热',10)  # 参数1:关键字，参数2:要下载的页数
+    getImg(dataList,'d:/python/img/') # 参数2:指定保存的路径
